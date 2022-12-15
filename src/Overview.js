40ef40fd47b13e7ff500fd3d991e7c30f72b5e03
Jack Rosa <jackrosa183@gmail.com>
2022-12-15 10:15:57 -0500

    added overview component

---
+++ src/Overview.js
@@ -0,0 +1,22 @@
+const Overview = ({user1Stats, user2Stats}) => {
+  const findExperiencedPlayer = (user1Total, user2Total) => {
+    const moreExperienced = Math.max(user1Total, user2Total)
+    if (moreExperienced === user1Total) {
+      return [user1Stats,user2Stats]
+    } else {
+      return [user2Stats,user1Stats]
+    }
+  }
+  const moreExperienced = findExperiencedPlayer(user1Stats.count.all, user2Stats.count.all);
+  const gameDifferential = () => {
+    return (moreExperienced[0].count.all - moreExperienced[1].count.all);
+  }
+  console.log("😂", gameDifferential());
+  return (
+    <div>
+      <p>{moreExperienced[0].id} is the more experienced player.</p>
+      <p>{moreExperienced[0].id} has played <strong>{gameDifferential()}</strong> more games than {moreExperienced[1].id}.</p>
+    </div>
+  );
+}
+export default Overview;
--- src/Stats.js
+++ src/Stats.js
@@ -1,36 +1,49 @@
+import Overview from './Overview'
+
 const Stats = ({user1Stats, user2Stats}) => {
   return (
-    <table className="stat-table">
-      <thead>
-        <tr>
-          <th>Statistic</th>
-          <th>{user1Stats.id}</th>
-          <th>{user2Stats.id}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Blitz Rating</td>
-          <td>{user1Stats.perfs.blitz.rating}</td>
-          <td>{user2Stats.perfs.blitz.rating}</td>
-        </tr>
-        <tr>
-          <td>Classical Rating</td>
-          <td>{user1Stats.perfs.classical.rating}</td>
-          <td>{user2Stats.perfs.classical.rating}</td>
-        </tr>
-        <tr>
-          <td>Rapid Rating</td>
-          <td>{user1Stats.perfs.rapid.rating}</td>
-          <td>{user2Stats.perfs.rapid.rating}</td>
-        </tr>
-        <tr>
-          <td>Total Games played</td>
-          <td>{user1Stats.count.all}</td>
-          <td>{user2Stats.count.all}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="container">
+      <h2>
+        Overview
+      </h2>
+      <Overview user1Stats={user1Stats} user2Stats={user2Stats}/>
+      <table className="stat-table">
+        <thead>
+          <tr>
+            <th>Statistic</th>
+            <th>{user1Stats.id}</th>
+            <th>{user2Stats.id}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Blitz Rating</td>
+            <td>{user1Stats.perfs.blitz.rating}</td>
+            <td>{user2Stats.perfs.blitz.rating}</td>
+          </tr>
+          <tr>
+            <td>Classical Rating</td>
+            <td>{user1Stats.perfs.classical.rating}</td>
+            <td>{user2Stats.perfs.classical.rating}</td>
+          </tr>
+          <tr>
+            <td>Rapid Rating</td>
+            <td>{user1Stats.perfs.rapid.rating}</td>
+            <td>{user2Stats.perfs.rapid.rating}</td>
+          </tr>
+          <tr>
+            <td>Puzzle Rating</td>
+            <td>{user1Stats.perfs.puzzle.rating}</td>
+            <td>{user2Stats.perfs.puzzle.rating}</td>
+          </tr>
+          <tr className="total">
+            <td>Total Games played</td>
+            <td>{user1Stats.count.all}</td>
+            <td>{user2Stats.count.all}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
